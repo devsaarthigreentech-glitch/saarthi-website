8144e8c2ae884c0a240b4729e18730e3c912744f
Aryan <dev.saarthigreentech@gmail.com>
2025-12-09 17:59:35 +0530

    Media section order

--- src/components/Resources/MediaSection/mediaData.ts
+++ src/components/Resources/MediaSection/mediaData.ts
@@ -11,6 +11,15 @@ export type MediaItem = {
 
 const mediaData: MediaItem[] = [
   {
+    id: 2,
+    title: "Hydrogen Retrofits, India's Most Scalable Decarbonisation Path: Saarthi GreenTech's Alok Kumar",
+    date: "December 09, 2025",
+    excerpt:
+      "At Saarthi Green, our approach is to keep the diesel engine architecture intact and layer a smart hydrogen system on top of it, said Alok Kumar, Director, Saarthi GreenTech, in an interview with Abha Rustagi, Associate Editor, Energetica India.",
+    image: "https://www.energetica-india.net/images/entrevistas/NTYj6ty7R0Msjok3daAYenksu1yUrBaLSDnXYZm1G2dExQ1EltwGWd.jpg",
+    link: "https://www.energetica-india.net/powerful-thoughts/online/alok-kumar",
+  },
+  {
     id: 1,
     title: "Green logistics: A shift towards sustainable future",
     date: "June 5, 2024",
@@ -19,15 +28,6 @@ const mediaData: MediaItem[] = [
     image: "/images/media/green-logistics.png",
     link: "https://cargoinsights.co/green-logistics-a-shift-towards-sustainable-future/",
   },
-  {
-    id: 2,
-    title: "Hydrogen Retrofits, India's Most Scalable Decarbonisation Path: Saarthi GreenTech's Alok Kumar",
-    date: "December 09, 2025",
-    excerpt:
-      "At Saarthi Green, our approach is to keep the diesel engine architecture intact and layer a smart hydrogen system on top of it, said Alok Kumar, Director, Saarthi GreenTech, in an interview with Abha Rustagi, Associate Editor, Energetica India.",
-    image: "https://www.energetica-india.net/images/entrevistas/NTYj6ty7R0Msjok3daAYenksu1yUrBaLSDnXYZm1G2dExQ1EltwGWd.jpg",
-    link: "https://www.energetica-india.net/powerful-thoughts/online/alok-kumar",
-  }
 ];
 
 export default mediaData;
